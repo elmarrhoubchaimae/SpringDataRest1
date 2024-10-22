35884145342ba31927fd314f496224180cf4068f
elmarrhoubchaimae <elmarrhoub.chaimae@gmail.com>
2024-10-22 22:34:47 +0100

    react

--- src/main/java/com/springdatarest1/cours/web/VoitureController.java
+++ src/main/java/com/springdatarest1/cours/web/VoitureController.java
@@ -3,16 +3,32 @@ package com.springdatarest1.cours.web;
 import com.springdatarest1.cours.modele.Voiture;
 import com.springdatarest1.cours.modele.VoitureRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/voitures") // Ajoutez une annotation de mapping de classe
 public class VoitureController {
+
     @Autowired
     private VoitureRepo voitureRepo;
-    @RequestMapping("/voitures")
-    public Iterable<Voiture> getVoitures(){
+
+    // Récupérer toutes les voitures
+    @GetMapping
+    public Iterable<Voiture> getVoitures() {
         return voitureRepo.findAll();
     }
+
+    // Ajouter une nouvelle voiture
+    @PostMapping // Ajoutez cette méthode pour gérer les requêtes POST
+    public Voiture createVoiture(@RequestBody Voiture voiture) {
+        return voitureRepo.save(voiture);
+    }
+
+    // Suppression d'une voiture par son ID
+    @DeleteMapping("/{id}")
+    public void deleteVoiture(@PathVariable Long id) {
+        voitureRepo.deleteById(id);
+    }
+
 }
